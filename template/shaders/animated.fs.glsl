888bcbe17f4450f98846a8ac1a251ff0da8cb389
ubcsabo <ubcsabo@students.cs.ubc.ca>
2023-12-01 21:07:14 -0800

    electrify powerups

--- template/shaders/animated.fs.glsl
+++ template/shaders/animated.fs.glsl
@@ -1,3 +1,5 @@
+
+
 #version 330
 
 // From vertex shader
@@ -8,9 +10,21 @@ uniform sampler2D sampler0;
 uniform vec3 fcolor;
 
 // Output color
-layout(location = 0) out  vec4 color;
+layout(location = 0) out vec4 color;
 
 void main()
 {
-	color = vec4(fcolor, 1.0) * texture(sampler0, vec2(texcoord.x, texcoord.y));
+    float electricityIntensity = 1000.f;
+    float electricityFrequency = 60.0;
+    float electricityOffset = sin(gl_FragCoord.x * electricityFrequency);
+    // Set electricity color to grayscale
+    vec3 electricityColor = vec3(0.0, 0.0, 0.001);
+    
+    vec4 textureColor = texture(sampler0, vec2(texcoord.x, texcoord.y));
+
+    // Add electricity effect to the final color
+    color = vec4(fcolor, 1.0) * textureColor;
+
+    color.xyz += electricityIntensity * electricityOffset * electricityColor;
 }
+
